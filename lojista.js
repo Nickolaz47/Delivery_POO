a0f479cb3dffd5a5ae05ecb2eab054b6b86d1bf7
Rodrigo Mendes <rodrigom.souza1999@gmail.com>
2022-03-24 21:29:04 -0300

    clean lojista

--- lojista.js
+++ lojista.js
@@ -1,18 +1,4 @@
-// O lojista poderá manipular o cardápio (adicionar, remover, alterar os pratos). 
-// Poderá, também, cancelar um pedido que esteja em andamento, ao qual esteja associado;
-// Ao finalizar um pedido, o lojista terá a sua lista de pedidos atualizada com este
-
 const Pessoa = require('./pessoa');
-const Cliente = require('./cliente');
-const Pedido = require('./pedido');
-const ItensCardapio = require('./itensCardapio');
-const PedidoInformado = require('./index');
-
-//const Cliente = importCliente.Cliente;
-//const Pedido = importCliente.Pedido;
-
-console.log(Pedido)
-
 
 class Lojista extends Pessoa {
     #senha
@@ -56,20 +42,6 @@ class Lojista extends Pessoa {
                 console.log("Pedido não associado")
             }
         }
-    /*   if(pedido.idLoja === this.idLoja) {
-            cliente.cancelarPedido(idPedido)
-            console.log("Pedido cancelado com sucesso")
-        }else {
-            console.log("Pedido não associado")
-        }
-         */
-        // console.log("Teste de cancelamento")
-        // console.log(Pedido.idLoja)
-        // if(Pedido.idLoja === this.idLoja) {
-        //     if(Pedido.pedidoRealizado === true && Pedido.pedidoFinalizado === true) {
-        //         Pedido.pedidoFinalizado === false
-        //     }
-        // }
     }
 
     confirmarPedido(cliente, idPedido) {
@@ -85,11 +57,6 @@ class Lojista extends Pessoa {
         }else{
             console.log('Pedido não encontrado.')
         }
-        /* if(pedido.idLoja === this.idLoja) {
-            if(pedido.id === idPedido) {
-                pedido.pedidoConfirmado = true
-            }
-        }  */
     }
 
     validaPedidos(pedidos){
@@ -114,35 +81,3 @@ class Lojista extends Pessoa {
 }
 
 module.exports = Lojista
-
-// Testes
-// const lojista = new Lojista("João","2003-02-13","12345678910","joaozinho@gmail.com",1,"1234");
-// const cliente = new Cliente('João', '2000-12-09', '12345678900', 'jao@mail.com', '123456');
-// const pedido = new Pedido(1, 1, 'big mac', 8, 2)
-// cliente.addItem(pedido)
-// cliente.realizarPedido()
-// cliente.finalizarPedido(1)
-
-// const item1 = new ItensCardapio("Pão",10)
-// const item2 = new ItensCardapio("Bife",20)
-// lojista.addItemCardapio(item1)
-// lojista.addItemCardapio(item2)
-// lojista.removerItemCardapio(item2)
-// lojista.cancelarPedidoEmAndamento()
-// console.log(cliente)
-
-// console.log(lojista)
-//console.log(cliente)
-
-
-// class Cardapio extends Pedido {
-//     constructor(idLoja,idCliente) {
-//         super(idLoja,idCliente)
-//         this.cardapio = cardapio
-//     }
-// }
-
-/* 
-console.log(pedido)
-console.log(pedido.idLoja)
- */
